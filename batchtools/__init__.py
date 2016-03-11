22a71a4f4abdc542091a7373601f42891f821bd7
Alex Drlica-Wagner <kadrlica@fnal.gov>
2016-03-10 22:11:15 -0600

    Initial commit

---
+++ batchtools/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+Package for working with distributed computing frameworks
+"""
+__author__ = "Alex Drlica-Wagner"
+__email__ = "kadrlica@fnal.gov"
+
+if __name__ == "__main__":
+    import argparse
+    description = __doc__
+    parser = argparse.ArgumentParser(description=description)
+    args = parser.parse_args()
---
+++ batchtools/lsf.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+Module for working with Platform LSF (c).
+"""
+
+if __name__ == "__main__":
+    import argparse
+    description = __doc__
+    parser = argparse.ArgumentParser(description=description)
+    args = parser.parse_args()
